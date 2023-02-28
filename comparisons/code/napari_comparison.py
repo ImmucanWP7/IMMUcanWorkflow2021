e0cb03f41bdf7063dbbb979a7d7af5be2bf321c2
nilseling <nils.eling@gmx.de>
2023-02-28 18:49:52 +0100

    Fixed napari plugin

--- comparisons/code/napari_comparison.py
+++ comparisons/code/napari_comparison.py
@@ -22,6 +22,8 @@ import napari
 import sys
 import re
 import os
+import numpy as np
+import pandas as pd
 from matplotlib import colors
 from xml.etree import ElementTree
 from vispy.color import Colormap
@@ -30,79 +32,132 @@ from qtpy.QtWidgets import QVBoxLayout, QWidget, QLabel, QHBoxLayout, QPushButto
 from qtpy.QtCore import Qt
 from qtpy.QtGui import QColor,QPixmap,QIcon
 
-class FileNavigator():
-    def __init__(self, folder) -> None:
+from steinbock.io import read_panel, read_image, read_mask
+
+class WidgetChannelsIntensity(QWidget):
+    def __init__(self,viewer,layer_names) -> None:
         super().__init__()
-        self.folder = folder
-
-class WidgetFileSelection(QWidget):
-    def __init__(self, viewer, folder) -> None:
-        super().__init__()
-        self._folder = folder
-        self._app = viewer 
-        self._current_index = 0
-
-        self._prev_button = QPushButton(parent=self)
-        self._prev_button.setIcon(
-            self.style().standardIcon(QStyle.StandardPixmap.SP_ArrowBack)
-        )
-        self._prev_button.clicked.connect(self._on_prev_button_clicked)
-
-        self._next_button = QPushButton(parent=self)
-        self._next_button.setIcon(
-            self.style().standardIcon(QStyle.StandardPixmap.SP_ArrowForward)
-        )
-        self._next_button.clicked.connect(self._on_next_button_clicked)
+        self.layer_names=layer_names
+        self.viewer=viewer
         
-        cur_img = os.path.splitext(os.listdir(self._folder + "/IMC" + "/img")[self._current_index])[0]
+        self.colordialog=QColorDialog()
+        #set custom colors
+        try:
+            color_list=[]
+            #add layer colors
+            col_ind=0
+            for i in range(len(self.layer_names)):
+                colRGB=self.viewer.layers[i].colormap.colors[1]
+                color=QColor(round(colRGB[0]*255), round(colRGB[1]*255),round(colRGB[2]*255))
+                if col_ind<self.colordialog.customCount() and not color.name() in color_list:
+                    self.colordialog.setCustomColor(col_ind,color)
+                    color_list.append(color.name())
+                    col_ind=col_ind+1
+            #add plain colors
+            for r in [0,255]:
+                for g in [0,255]:
+                    for b in [0,255]:
+                        color=QColor(r,g,b)
+                        if col_ind<self.colordialog.customCount() and not color.name() in color_list:
+                            self.colordialog.setCustomColor(col_ind,color)
+                            color_list.append(color.name())
+                            col_ind=col_ind+1
+        except:
+            print('cannot set custom colors')
+      
+        #store original contrast limits
+        self.contrast_limits=[]
+        for i in range(len(self.layer_names)):
+            self.contrast_limits.append(self.viewer.layers[self.layer_names[i]].contrast_limits)
 
-        layout = QVBoxLayout()
+        self.setLayout(QVBoxLayout())
 
-        files_box = QLabel()
-        files_box.setText(cur_img)
-        layout.addWidget(files_box)
+        self.buttons_showhide=[]
+        for i in range(len(self.layer_names)):
+            self.buttons_showhide.append(QPushButton("Hide"))
+            self.buttons_showhide[i].setCheckable(True)
+            self.buttons_showhide[i].setChecked(True)
+            self.buttons_showhide[i].clicked.connect(self.btn_show_hide_click)
 
-        button_layout = QHBoxLayout()
-        button_layout.addWidget(self._prev_button)
-        button_layout.addWidget(self._next_button)
-        layout.addLayout(button_layout)
+        self.buttons_color=[]
+        for i in range(len(self.layer_names)):
+            self.buttons_color.append(QPushButton())
+            self.buttons_color[i].clicked.connect(self.btn_color_click)
+            colRGB=self.viewer.layers[i].colormap.colors[1]
+            colname=QColor(round(colRGB[0]*255), round(colRGB[1]*255),round(colRGB[2]*255)).name()
+            self.buttons_color[i].setStyleSheet("QPushButton{ background-color: "+colname+" }")
+            
+        self.sliders=[]
+        self.slider_lim=100
+        for i in range(len(self.layer_names)):
+            self.sliders.append(QSlider(Qt.Horizontal))
+            self.sliders[i].setMinimum(-self.slider_lim)
+            self.sliders[i].setMaximum(self.slider_lim)
+            self.sliders[i].setValue(0)
+            self.sliders[i].valueChanged.connect(self.slider_change)
+            
+        for i in range(len(self.layer_names)):
+            self.layout().addWidget(QLabel(self.layer_names[i]+":"))
+            tmplayout=QHBoxLayout()
+            tmplayout.addWidget(self.buttons_showhide[i])
+            tmplayout.addWidget(self.buttons_color[i])
+            tmplayout.addWidget(self.sliders[i])
+            self.layout().addLayout(tmplayout)
 
-        self.setLayout(layout)
+        #add spacer (when the widget is inside a QScrollArea)
+        self.layout().addStretch(1)
         
-    def _on_prev_button_clicked(self, checked: bool = False) -> None:
-        self._current_index = self._current_index - 1
+    def slider_change(self):
+        for i in range(len(self.layer_names)):
+            if self.sender()==self.sliders[i]:
+                max_factor=50
+                x=max_factor**(-self.sliders[i].value()/self.slider_lim)
+                self.viewer.layers[self.layer_names[i]].contrast_limits=(self.contrast_limits[i][0],self.contrast_limits[i][1]*x)
 
-    def _on_next_button_clicked(self, checked: bool = False) -> None:
-        self._current_index = self._current_index + 1
+    def btn_show_hide_click(self):
+        for i in range(len(self.layer_names)):
+            if self.sender()==self.buttons_showhide[i]:
+                if self.buttons_showhide[i].isChecked():
+                    self.buttons_showhide[i].setText("Hide")
+                    self.viewer.layers[self.layer_names[i]].visible=True
+                    self.sliders[i].setEnabled(True)
+                else:
+                    self.buttons_showhide[i].setText("Show")
+                    self.viewer.layers[self.layer_names[i]].visible=False
+                    self.sliders[i].setEnabled(False)
 
-
-def napari_comparison_test(folder) -> None:
-    
-    viewer1 = napari.Viewer(title=folder)
-
-    #hide "layer controls" and "layer list" docks
-    viewer1.window._qt_viewer.dockLayerControls.hide()
-    viewer1.window._qt_viewer.dockLayerList.hide()
-    
-    selector = QScrollArea()
-    selector.setWidgetResizable(True)
-    selector.setWidget(WidgetFileSelection(viewer1, folder))
-    viewer1.window.add_dock_widget(selector, area = 'right', name='Select sample')
-    
-    napari.run()
+    def btn_color_click(self):
+        for i in range(len(self.layer_names)):
+            if self.sender()==self.buttons_color[i]:
+                #get layer color
+                colRGB=self.viewer.layers[i].colormap.colors[1]
+                color=QColor(round(colRGB[0]*255), round(colRGB[1]*255),round(colRGB[2]*255))
+                color=self.colordialog.getColor(initial=color)
+                if color.isValid():
+                    #set layer color
+                    cm=self.viewer.layers[i].colormap
+                    cm.update({'colors':[cm.colors[0],[color.redF(),color.greenF(),color.blueF(),color.alphaF()]]})
+                    self.viewer.layers[i].colormap=cm
+                    #set button color
+                    self.buttons_color[i].setStyleSheet("QPushButton{ background-color: "+color.name()+" }")
     
 
 ###########################################
 # main function
 ###########################################
-def napari_comparison(imc_image, 
-                      imc_dir,
-                      imc_panel, 
-                      imc_cells,
-                      mIF_image,
-                      mIF_dir,
-                      mIF_panel,
-                      mIF_cells) -> None:
+def napari_comparison(path, sample_name) -> None:
+    
+    path_to_imc = path + "/IMC/"
+    path_to_mIF = path + "/mIF/"
+    
+    imc_image = read_image(path_to_imc + "img/" + sample_name + ".tiff")
+    imc_dir = path_to_imc + "img/"
+    imc_panel = read_panel(path_to_imc + "panel.csv")
+    imc_cells = pd.read_table(path_to_imc + "cells/" + sample_name + ".tsv")
+    mIF_image = read_image(path_to_mIF + "img/" + sample_name + ".tiff")
+    mIF_dir = path_to_mIF + "img/"
+    mIF_panel = read_panel(path_to_mIF + "panel.csv")
+    mIF_cells = pd.read_table(path_to_mIF + "cells/" + sample_name + ".tsv")
 
     ####################
     # viewer1 for mIF
@@ -249,7 +304,7 @@ def napari_comparison(imc_image,
                 mIF_all_cells.refresh()
                 mIF_all_cells.visible=True  #make sure it is visible
     
-    channel_names_tmp=channel_names[1:-1] #ignore DAPI and autofluorescence
+    channel_names_tmp=["CD15", "CK", "CD3", "CD11c", "CD20", "CD163"] #ignore DAPI and autofluorescence
     scrollArea = QScrollArea()
     scrollArea.setWidgetResizable(True)
     scrollArea.setWidget(WidgetHighlightNuclei(None,
@@ -260,10 +315,10 @@ def napari_comparison(imc_image,
     ################################
     # Widget for selecting samples #
     ################################
-    selector = QScrollArea()
-    selector.setWidgetResizable(True)
-    selector.setWidget(WidgetFileSelection(viewer1, "test"))
-    viewer1.window.add_dock_widget(selector, area = 'right', name='Select sample')
+    #selector = QScrollArea()
+    #selector.setWidgetResizable(True)
+    #selector.setWidget(WidgetFileSelection(viewer1, "test"))
+    #viewer1.window.add_dock_widget(selector, area = 'right', name='Select sample')
     
     ####################
     # viewer2 for IMC
@@ -307,7 +362,7 @@ def napari_comparison(imc_image,
 
 
     #add image (scaled to match img1 size), using enlarged contrast_limits to fix slider limits
-    imc_scale=(mIF_image.shape[0]/imc_image[0].shape[0]+mIF_image.shape[1]/imc_image[0].shape[1])/2
+    imc_scale=(mIF_image.shape[1]/imc_image.shape[1]+mIF_image.shape[2]/imc_image.shape[2])/2
     viewer2.add_image(imc_image,
                       channel_axis=0,
                       name=layer_names,
@@ -319,14 +374,14 @@ def napari_comparison(imc_image,
                      )
 
     # #link views: does not work well. Seems to automatically change zoom sometimes... Problem seems to be solved in napari v0.4.16
-    # def viewer1_camera_event(event: napari.utils.events.Event):
-    #     viewer2.camera.update(viewer1.camera)
-    # 
-    # def viewer2_camera_event(event: napari.utils.events.Event):
-    #     viewer1.camera.update(viewer2.camera)
-    # 
-    # viewer1.camera.events.connect(viewer1_camera_event)
-    # viewer2.camera.events.connect(viewer2_camera_event)
+    def viewer1_camera_event(event: napari.utils.events.Event):
+         viewer2.camera.update(viewer1.camera)
+     
+    def viewer2_camera_event(event: napari.utils.events.Event):
+         viewer1.camera.update(viewer2.camera)
+     
+    viewer1.camera.events.connect(viewer1_camera_event)
+    viewer2.camera.events.connect(viewer2_camera_event)
     # #see also
     # #https://forum.image.sc/t/different-ways-to-control-camera-in-napari/60552
     # #https://github.com/napari/napari/issues/3723
@@ -337,7 +392,7 @@ def napari_comparison(imc_image,
 
 
     #Simpler workaround (Public access to Window.qt_viewer is deprecated and will be removed in napari v0.5.0)
-    viewer2.window.qt_viewer.view.camera.link(viewer1.window.qt_viewer.view.camera)
+    #viewer2.window.qt_viewer.view.camera.link(viewer1.window.qt_viewer.view.camera)
 
     ###############################
     # IMC Nuclei
@@ -346,7 +401,7 @@ def napari_comparison(imc_image,
     IMC_cell_centers = imc_cells[["Pos_Y", "Pos_X"]] #order (y,x)
     
     # phenotype
-    IMC_cell_phenotype = imc_cells["celltypes"]
+    IMC_cell_phenotype = imc_cells["celltype"]
     IMC_cell_type_matched = imc_cells["matched_celltype"]
 
     IMC_phenotypes = sorted(list(set(IMC_cell_phenotype)))
